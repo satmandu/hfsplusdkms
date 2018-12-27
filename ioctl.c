1c37a287a4f2feb88932fc4ca8623bacf26f7c1f
satmandu <satadru@umich.edu>
2018-12-27 13:50:40 -0500

    no CURRENT_TIME_SEC
    adapted from https://github.com/torvalds/linux/blob/master/fs/ufs/namei.c

--- ioctl.c
+++ ioctl.c
@@ -124,7 +124,7 @@ static int hfsplus_ioctl_setflags(struct file *file, int __user *user_flags)
 	else
 		hip->userflags &= ~HFSPLUS_FLG_NODUMP;
 
-	inode->i_ctime = CURRENT_TIME_SEC;
+	inode->i_ctime = current_time(inode);
 	mark_inode_dirty(inode);
 
 out_unlock_inode:
